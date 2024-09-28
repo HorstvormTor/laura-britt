2479c6077fb3c7041cc89456ee8b95c642be867b
laura <info@laurabritt.de>
2024-09-28 12:45:12 +0200

    first go-live

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function fadeIn() {
     }
 }
 fadeIn();
+
+function cookieBannerSaveConsent(e) {
+    document.cookie = "gdprConsent=true; path=/;"
+    checkHideCookieBanner()
+    // e.preventDefault();
+}
+
+function checkHideCookieBanner() {
+    let cookie = document.cookie.split(";").find((cookie) => cookie.trim().startsWith("gdprConsent="))
+    if (cookie !== undefined && cookie.trim() === "gdprConsent=true") {
+        const banner = document.querySelector(".cookie-banner-outer");
+        banner.classList.add("hidden")
+    }
+}
+
+window.addEventListener('load', checkHideCookieBanner)
